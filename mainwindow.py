c67f1a09b806743a4e2e64373e35c220ca0f62d8
Pratik Sinha <53615098+beasthunter758@users.noreply.github.com>
2020-03-29 14:08:02 +0530

    completed mainwindow

--- mainwindow.py
+++ mainwindow.py
@@ -16,4 +16,21 @@ class Window(QMainWindow):
         self._createStatusbar()
 
         def _createMenu(self):
-            
+            self.menu = self.menuBar().addMenu("&Menu")
+            self.menu.addAction('&Exit', self.close)
+
+        def _createToolBar(self):
+            tools = QToolBar()
+            self.addToolbar(tools)
+            tools.addAction('Exit', self.close)
+
+        def _createStatusbar(self):
+            status = QStatusBar()
+            status.showMessage("I'm the Status Bar")
+            self.setStatusbar(status)
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
